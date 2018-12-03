e93f96d04f78ab08b85befe929579fdf75e21ab1
nnhai1991@gmail.com <Hai Nguyen>
2018-12-03 22:51:37 +0800

    add backend

--- python-backend/api.py
+++ python-backend/api.py
@@ -3,20 +3,25 @@ from flask import Flask, jsonify
 from flask import request
 from flask import make_response
 from flask import abort
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/api/ask', methods=['POST'])
+@cross_origin()
 def api_ask():
     if not request.json or not 'user_message' in request.json:
         abort(400)
 
     question =  request.json['user_message'],
     answer = {
+        'type':"answer",
         'question' : question,
-        'answer' : "i'm a poor boy from a poor family"
+        'message' : "I'm a poor boy from a poor family. Easy come, easy go, will you let me go?"
     }
-    return jsonify(answer), 201
+    return jsonify(answer), 200
 
 @app.errorhandler(404)
 def not_found(error):
